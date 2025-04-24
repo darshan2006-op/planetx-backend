43e3e270bfdff511db9e1e8ed2a66c344ffae963
Raghuraj Pratap Singh <Raghurajpratap2810@gmail.com>
2025-04-25 00:11:57 +0530

    exptend limit

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use(function (req, res, next) {
 });
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 
 const startServer = async () => {
